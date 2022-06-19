e187d0cf49ed50d038896b6aaf186bf6ff8bb12e
xr-auth <107809181+xr-auth@users.noreply.github.com>
2022-06-19 18:46:54 +0100

    Create games.lua

---
+++ xr/games.lua
@@ -0,0 +1,12 @@
+local games = {
+	[5278850819] = {
+		name = "Stay Alive And Flex Your Time On Others",
+		loader = "",
+		patched = false;
+	};
+	[185655149] = {
+		name = "Welcome To Bloxburg",
+		loader = "",
+		patched = false;
+	};
+};
